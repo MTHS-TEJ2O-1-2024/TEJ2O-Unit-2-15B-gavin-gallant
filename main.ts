6d4a0aeadfd1b03530db358600c24d89b766a63c
gavin-gallant <gavin.gallant@mths.ca>
2024-12-08 17:52:03 -0500

    made the code

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: nov 2024
+ * This program moves led in a circle
 */
 
-basic.showString('Hello, World!')
+
+// variables
+let sprite: game.LedSprite = null
+let loopCounterbox = 0
+let loopCounterside = 0
+
+// setup
+basic.pause(500)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+// when "A" is pressed, the pixels move in a squaire  
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounterbox = 0
+    loopCounterside = 0
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.X, loopCounterside)
+    // move right 
+    while (loopCounterbox <= 3) {
+        while (loopCounterside <= 5) {
+            sprite.move(1)
+            loopCounterside++
+            basic.pause(500)
+        }
+        sprite.turn(Direction.Right, 90)
+        loopCounterside = 0
+        loopCounterbox++
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
