6ff3de6c540dd48696d12f4c420c9eb2dcfe4304
gavin-gallant <gavin.gallant@mths.ca>
2024-12-16 15:53:53 +0000

    updated code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,8 +1,9 @@
 """
 Gavin Gallant
-Created on: nov 2024
-This program moves led in a circle
+Created on: Nov 2024
+This program moves an LED in a square pattern.
 """
+
 from microbit import *
 
 # Initial setup
@@ -10,35 +11,44 @@ sleep(500)
 display.clear()
 display.show(Image.HAPPY)
 
+# Function to move the LED in a square pattern
 def move_led_in_square():
-    display.clear()
-    sprite_x = 0
+    display.clear()  # Clear the screen at the start
+    sprite_x = 0  # Starting position of the LED
     sprite_y = 0
 
-    loop_counter_box = 0
-    while loop_counter_box < 4:  # Loop for each side of the square
-        loop_counter_side = 0
-        while loop_counter_side < 5:  # Loop for moving along a side
-            display.set_pixel(sprite_x, sprite_y, 9)  # Turn on LED
-            sleep(500)
-            display.set_pixel(sprite_x, sprite_y, 0)  # Turn off LED
+    # Variable to track which side of the square is being drawn
+    current_side = 0
 
-            # Move the sprite in the current direction
-            if loop_counter_box == 0:  # Move right
+    # Repeat for all four sides of the square
+    while current_side < 4:
+        step_in_side = 0  # Variable to track steps along one side
+
+        # Move the LED 5 times along the current side
+        while step_in_side < 5:
+            display.set_pixel(sprite_x, sprite_y, 9)  # Turn on the LED
+            sleep(500)  # Wait
+            display.set_pixel(sprite_x, sprite_y, 0)  # Turn off the LED
+
+            # Decide the direction to move based on the current side
+            if current_side == 0:  # First side: move right
                 sprite_x += 1
-            elif loop_counter_box == 1:  # Move down
+            elif current_side == 1:  # Second side: move down
                 sprite_y += 1
-            elif loop_counter_box == 2:  # Move left
+            elif current_side == 2:  # Third side: move left
                 sprite_x -= 1
-            elif loop_counter_box == 3:  # Move up
+            elif current_side == 3:  # Fourth side: move up
                 sprite_y -= 1
 
-            loop_counter_side += 1
-        loop_counter_box += 1
+            step_in_side += 1  # Move to the next step in the side
 
-    display.show(Image.HAPPY)  # Show happy face after the loop
+        current_side += 1  # Move to the next side of the square
 
-# Event loop
+    # Show a happy face after completing the square
+    display.show(Image.HAPPY)
+
+# Main loop to wait for button press
 while True:
+    # Start the LED movement when button A is pressed
     if button_a.was_pressed():
         move_led_in_square()
